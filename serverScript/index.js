c9e0bee3f4de3cabfebc7d2de853105f639b691c
Alex Memering <memering@firebase.com>
2015-06-23 09:35:52 -0700

    Server script removes commands as it processes them.

--- serverScript/index.js
+++ serverScript/index.js
@@ -10,22 +10,19 @@ var commandsFb = machineFb.child('commands');
 var historyFb = machineFb.child('history');
 
 function executeCommand(command, logFunction) {
-  console.log('command: ' + command);
   exec(command, logFunction);
-};
+}
 
 function logToFirebase(error, stdout, stderr) {
-  console.log('error: ' + error);
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-  if (error){
-    historyFb.push({"error": error});
-  } else{
-    historyFb.push({"stdout": stdout, "stderr": stderr});
+  if (error) {
+    historyFb.push({'error': error});
+  } else {
+    historyFb.push({'stdout': stdout, 'stderr': stderr});
   }
-};
+}
 
 commandsFb.on('child_added', function(snapshot, prevChildKey) {
   var command = snapshot.val();
   executeCommand(command, logToFirebase);
+  snapshot.ref().remove();
 });
