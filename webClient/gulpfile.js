ecd09907ba0498bd7043d919647801400e26aaae
Alex Memering <memering@firebase.com>
2015-06-23 12:01:11 -0700

    Libraries work on delpoyment

--- webClient/gulpfile.js
+++ webClient/gulpfile.js
@@ -7,12 +7,24 @@ var runSequence = require('run-sequence');
 
 
 var paths = {
-  js: [],
+  js: [
+    'src/**/*.js'
+  ],
   html: [
     'src/**/*.html'
   ],
   tests: [],
-  dest: 'build'
+  dest: 'build',
+  libs: {
+    js: [
+      'bower_components/angular/angular.js',
+      'bower_components/angularfire/dist/angularfire.js',
+      'bower_components/firebase/firebase.js'
+    ],
+    css: [
+      'bower_components/bootstrap/dist/css/bootstrap.css'
+    ]
+  }
 };
 
 
@@ -29,11 +41,23 @@ gulp.task('html', function() {
     .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.html, ['html']);
+gulp.task('js', function() {
+  var jsFiles = _.union(paths.js, paths.libs.js);
+  return gulp.src(jsFiles)
+    .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('build', ['html']);
+gulp.task('css', function() {
+  return gulp.src(paths.libs.css)
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.js, ['js']);
+});
+
+gulp.task('build', ['html', 'js', 'css']);
 
 gulp.task('default', function(done) {
   runSequence('lint', 'build', function(error) {
---
+++ webClient/src/project.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('project', ['firebase'])
+  .controller('StartController', function() {
+    this.data = 'Some data';
+  });
