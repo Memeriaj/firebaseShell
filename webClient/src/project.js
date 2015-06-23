4b6029a01bda67b8f916e2ba047e403f49984f77
Alex Memering <memering@firebase.com>
2015-06-23 13:41:15 -0700

    History is now displayed.

--- webClient/src/project.js
+++ webClient/src/project.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('project', ['firebase'])
-  .controller('StartController', function() {
-    this.data = 'Some data';
-  });
+  .value('fbRoot', 'https://fb-shell.firebaseio.com/')
+  .service('rootRef', function(fbRoot) {
+    return new Firebase(fbRoot);
+  })
+  .controller('HistoryController', ['$firebaseArray', 'rootRef', function($firebaseArray, rootRef) {
+    var machine = 'tester';
+    var numLines = 12;
+
+    var histRef = rootRef.child('/machines/' + machine + '/history/');
+    this.commands = $firebaseArray(histRef.limitToLast(numLines));
+  }]);
