5fcdb462301ad8c677aaec7b2901ebe4eac6857f
Alex Memering <memering@firebase.com>
2015-06-23 18:06:34 -0700

    Web view has a way to enter commands.

--- webClient/src/project.js
+++ webClient/src/project.js
@@ -11,4 +11,18 @@ angular.module('project', ['firebase'])
 
     var histRef = rootRef.child('/machines/' + machine + '/history/');
     this.commands = $firebaseArray(histRef.limitToLast(numLines));
+
+    this.correctNewline = function(str) {
+      return str.trim().split('\n');
+    };
+  }])
+  .controller('CommandController', ['$firebaseArray', 'rootRef', function($firebaseArray, rootRef) {
+    var machine = 'tester';
+    var commandRef = rootRef.child('/machines/' + machine + '/commands/');
+    var commandArray = $firebaseArray(commandRef);
+
+    this.sendCommand = function() {
+      commandArray.$add(this.newCommand);
+      this.newCommand = '';
+    };
   }]);
